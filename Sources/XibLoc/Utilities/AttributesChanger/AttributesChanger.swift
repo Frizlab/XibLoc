c6bec3aeab5ee6cb2d68fe9db35c4eb6675a99de
Frizlab <fload@me.com>
2025-11-02 23:16:26 +0100

    Remove some useless imports

--- Sources/XibLoc/Utilities/AttributesChanger/AttributesChanger.swift
+++ Sources/XibLoc/Utilities/AttributesChanger/AttributesChanger.swift
@@ -14,11 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 import Foundation
-#if os(macOS)
-import AppKit
-#elseif canImport(UIKit)
-import UIKit
-#endif
 
 
 
